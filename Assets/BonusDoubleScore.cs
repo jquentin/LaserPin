5f9dae69b0ea9beb95dafcc6939abcc14545a65b
Jeremy Quentin <jeremy.quentin@gmail.com>
2020-06-05 20:51:35 +0200

    Leftover changes

---
+++ Assets/BonusDoubleScore.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusDoubleScore : BonusComponent {
+
+	public int time;
+
+
+}
---
+++ Assets/BonusManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BonusComponent : MonoBehaviour
+{
+}
+
+[Serializable]
+public class Bonus
+{
+
+	public int price;
+
+	public Texture icon;
+
+	public BonusComponent component;
+
+}
+
+public class BonusManager : MonoBehaviour 
+{
+
+	#region Singleton
+	private static BonusManager _instance;
+	public static BonusManager instance
+	{
+		get
+		{
+			if (_instance == null)
+				_instance = FindObjectOfType<BonusManager>();
+			return _instance;
+		}
+	}
+	#endregion
+
+	public List<Bonus> bonuses;
+
+}
